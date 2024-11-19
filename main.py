e5ea36ec2e66ab8c8d18568ce9fc40a8d6105ab3
zaw win aung <zawaung7791@users.noreply.github.com>
2024-11-19 14:31:08 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import streamlit as st
 import pandas as pd 
 import plotly.express as px
 
-#st.set_page_config(layout="wide")
+st.set_page_config(layout="wide")
 
-st.title("Streamlit Test 1")     
+st.title("Quick Data Previewer")     
 
 st.header("File")   
 
@@ -15,54 +15,71 @@ uploaded_file = st.file_uploader("Choose a file", type=['xlsx', 'csv'])
 if uploaded_file is not None:
     # st.write(uploaded_file.name + " has been uploaded.")   
     
-    df = pd.read_csv(uploaded_file)
+    # handle if csv or excel file loaded
 
-    # a label, "Data shape"
-    st.text("Data profile")
-    st.write(df.shape)
-    st.write(df.describe())
-    st.text("DataFrame")
-    st.dataframe(df)
+    if uploaded_file.name.endswith('.xlsx'):
+        df = pd.read_excel(uploaded_file)   
+    else:
+        df = pd.read_csv(uploaded_file)
 
-    # save the dataframe in session state
-    st.session_state['df'] = df
     numeric_df = df.select_dtypes(include=['number'])
 
-    st.header("Graphs")
+    category_df = df.select_dtypes(include=['object'])
 
-    # a st dropdown to select the column
-    column = st.selectbox("Select a column for histogram", numeric_df.columns)
-    fig = px.histogram(df, x=column)
-    st.plotly_chart(fig)
+    col1, col2 = st.columns(2)
 
-    # draw a box plot of only the numeric columns
-    st.text("Box Plot")
-    column2 = st.selectbox("Select a column for box plot", numeric_df.columns)
-    fig = px.box(df, y=column2)
-    st.plotly_chart(fig)
+    with col1:
+        # a label, "Data profile"
+        st.text("Data profile")
+        st.write(df.shape)
 
-    # draw a heatmap of only the numeric columns
-    st.text("Correlation Heatmap")
-    fig = px.imshow(numeric_df.corr())
-    st.plotly_chart(fig)
+        # Display the dataframe
+        st.text("Data preview")
+        st.write(df.head())
 
-    # draw a scatter plot of only the numeric columns
-    # allow the user to select the x and y columns
-    # show regression, the r squared value, p value and equation of the line
-    st.text("Scatter Plot")
-    x_column = st.selectbox("Select x column", numeric_df.columns)
-    y_column = st.selectbox("Select y column", numeric_df.columns)
-    add_regression = st.checkbox("Add regression line")
-    fig = px.scatter(df, x=x_column, y=y_column, trendline="ols" if add_regression else None)
-    st.plotly_chart(fig)
+        # Display the data types
+        st.text("Summary stats")
+        st.write(df.describe())
 
-    # draw a pie chart, let the user choose the category (a non-numeric column) and the value (a numeric column)
-    st.text("Pie Chart")
-    category_column = st.selectbox("Select category column", df.select_dtypes(include=['object']).columns)
-    value_column = st.selectbox("Select value column", numeric_df.columns)
-    fig = px.pie(df, names=category_column, values=value_column)
-    st.plotly_chart(fig)
-    
+        # Display the numerical columns
+        st.text("Numerical columns")
+        st.table([numeric_df.columns, numeric_df.dtypes])
 
-else:
-    st.write("Please upload a file.")
+        # Display the categorical columns
+        st.text("Categorical columns")
+        st.table([category_df.columns, category_df.dtypes])
+
+
+    with col2:
+
+        # display a histogram
+        st.text("Histogram")
+        column1 = st.selectbox("Select a column for histogram", df.select_dtypes(include=['float64', 'int64']).columns)
+        fig = px.histogram(df, x=column1)
+        st.plotly_chart(fig)
+
+        # Display a box plot
+        st.text("Box Plot")
+        column2 = st.selectbox("Select a column for box plot", df.select_dtypes(include=['float64', 'int64']).columns)
+        fig = px.box(df, y=column2)
+        st.plotly_chart(fig)
+
+        # Display a correlation heatmap
+        st.text("Correlation Heatmap")
+        fig = px.imshow(numeric_df.corr())
+        st.plotly_chart(fig)
+
+        # Display a scatter plot
+        st.text("Scatter Plot")
+        x_column = st.selectbox("Select x column", df.select_dtypes(include=['float64', 'int64']).columns)
+        y_column = st.selectbox("Select y column", df.select_dtypes(include=['float64', 'int64']).columns)
+        add_regression = st.checkbox("Add regression line")
+        fig = px.scatter(df, x=x_column, y=y_column, trendline="ols" if add_regression else None)
+        st.plotly_chart(fig)
+
+        # Display a pie chart
+        st.text("Pie Chart")
+        category_column = st.selectbox("Select category column", df.select_dtypes(include=['object']).columns)
+        value_column = st.selectbox("Select value column", df.select_dtypes(include=['float64', 'int64']).columns)
+        fig = px.pie(df, names=category_column, values=value_column)
+        st.plotly_chart(fig)
